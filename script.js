c1ba1d414749612f8632fd116cb7f9fdb6b582b4
joaoobzutmeira-del <joao.obzut.meira@escola.pr.gov.br>
2026-09-18 10:32:32 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventeListener9("click", curtir);
+function curtir(){
+    const contador = botapCutir.querySelector("span");
+    if(curtiu === false){
+        contador.textContent++;
+        curtiu = true;}
+        else{
+            contador.textContent--;
+            curtiu = false;
+        }
+}
+});
